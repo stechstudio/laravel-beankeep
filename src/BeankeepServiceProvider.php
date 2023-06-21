bf54338e7e63e1c988835eebcf8c6bc51f2444ec
Zeb DeOs <zebulin@stechstudio.com>
2023-06-21 17:10:27 -0400

    BeankeepServiceProvider: stub out service provider

---
+++ src/BeankeepServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace STS\LaravelBeankeep;
+
+use Illuminate\Support\ServiceProvider;
+
+class BeankeepServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+    }
+}
