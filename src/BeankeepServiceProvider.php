2e9736da576b9d6cb98a984ce8b776f8bc2b2693
Zeb DeOs <zebulin@stechstudio.com>
2023-06-22 17:14:57 -0400

    BeankeepServiceProvider: fix path pointing to migrations

--- src/BeankeepServiceProvider.php
+++ src/BeankeepServiceProvider.php
@@ -8,6 +8,6 @@ class BeankeepServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__.'../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
