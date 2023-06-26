a5c3a180ed9f0a3e0e669a4d9d17077eb7ba93e9
Zeb DeOs <zebulin@stechstudio.com>
2023-06-26 15:29:20 -0400

    migrations/create_beankeep_accounts_table: notes with alex on using polymorphic relations to manage link between library-provided models/data and app/user models/data

--- database/migrations/2022_12_19_000000_create_beankeep_accounts_table.php
+++ database/migrations/2022_12_19_000000_create_beankeep_accounts_table.php
@@ -19,6 +19,29 @@ return new class extends Migration {
             $table->string('type');
             $table->string('number')->unique();
 
+            // TODO(zmd): polymorphic: keepable_id, keepable_type (class
+            //   string)
+            //
+            //  bootable trait
+            //
+            // Beankeep/Models/Account::find(1)->keepable;
+            //    <-- App/Models/HomeownerAccount() // -> use Keepable;
+            //
+            // HomeownerAccount::find(1)->getMorphClass(); -> (keepable_type)
+            //
+            // beankeep:make:keepable HomeownerAccount
+            // contract/interface, trait
+            //   -> getBeankeepFields();
+            // class User implements IsBeankeepAccount
+            // {
+            //    use Keepable;
+            //
+            //    public function getBeankeepFields(): array
+            //    {}
+            // }
+            //
+            // update event, automatically deal with audit log / block
+
             $table->timestamps();
         });
     }
