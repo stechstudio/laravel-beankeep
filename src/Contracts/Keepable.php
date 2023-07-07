ab685291b18d41fbccd137a11e6a6692db82200c
Zeb DeOs <zebulin@stechstudio.com>
2023-07-07 12:01:28 -0400

    Keepable: let's make a deal

---
+++ src/Contracts/Keepable.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace STS\Beankeep\Contracts;
+
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+interface Keepable
+{
+    public function keeper(): MorphOne;
+
+    public function keepAttributes(): array;
+}
