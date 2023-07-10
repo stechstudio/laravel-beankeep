d83fcf508a99766f25e151337916ac37f3216cb5
Zeb DeOs <zebulin@stechstudio.com>
2023-07-10 16:14:38 -0400

    TestCase: load our service provider

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,14 @@
 namespace STS\Beankeep\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use STS\Beankeep\BeankeepServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function getPackageProviders($app): array
+    {
+        return [
+            BeankeepServiceProvider::class,
+        ];
+    }
 }
