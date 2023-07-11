cccd9dfec7d373824344c31ac96fc63e7aa48a44
Zeb DeOs <zebulin@stechstudio.com>
2023-07-11 10:45:46 -0400

    LineItemTest: if you're gonna use datasets, actually use their values in your tests LOL!

--- tests/Unit/LineItemTest.php
+++ tests/Unit/LineItemTest.php
@@ -32,9 +32,9 @@ it('can convert debit amount from cents to dollars', function (
     int $amountInCents,
     float $amountInDollars,
 ) {
-    $lineItem = new LineItem(['debit' => 133742, 'credit' => 0]);
+    $lineItem = new LineItem(['debit' => $amountInCents, 'credit' => 0]);
 
-    expect($lineItem->debitInDollars())->toBe(1337.42);
+    expect($lineItem->debitInDollars())->toBe($amountInDollars);
 })->with([
     [100, 1.0],
     [133742, 1337.42],
