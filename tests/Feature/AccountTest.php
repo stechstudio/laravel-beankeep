1b228090cc80466a314ab870c7929b6dc61f1d0c
Zeb DeOs <zebulin@stechstudio.com>
2023-07-11 12:15:22 -0400

    AccountTest: use the power of loops

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -5,56 +5,28 @@ declare(strict_types=1);
 use STS\Beankeep\Enums\AccountType;
 use STS\Beankeep\Models\Account;
 
-function createAccounts() {
-    Account::create([
-        'number' => '1000',
-        'type' => AccountType::Asset,
-        'name' => 'Assets',
-    ]);
-    Account::create([
-        'number' => '2000',
-        'type' => AccountType::Liability,
-        'name' => 'Liabilities',
-    ]);
-    Account::create([
-        'number' => '3000',
-        'type' => AccountType::Equity,
-        'name' => 'Equity',
-    ]);
-    Account::create([
-        'number' => '4000',
-        'type' => AccountType::Revenue,
-        'name' => 'Income',
-    ]);
-    Account::create([
-        'number' => '5000',
-        'type' => AccountType::Expense,
-        'name' => 'Expenses',
-    ]);
-}
-
 it('can model the chart of accounts', function () {
-    createAccounts();
+    $accountAttributes = [
+        ['1000', AccountType::Asset, 'Assets'],
+        ['2000', AccountType::Liability, 'Liabilities'],
+        ['3000', AccountType::Equity, 'Equity'],
+        ['4000', AccountType::Revenue, 'Income'],
+        ['5000', AccountType::Expense, 'Expenses'],
+    ];
+
+    foreach ($accountAttributes as [$number, $type, $name]) {
+        Account::create([
+            'number' => $number,
+            'type' => $type,
+            'name' => $name,
+        ]);
+    }
 
     $accounts = Account::all();
 
-    expect($accounts[0]->number)->toBe('1000');
-    expect($accounts[0]->type)->toEqual(AccountType::Asset);
-    expect($accounts[0]->name)->toBe('Assets');
-
-    expect($accounts[1]->number)->toBe('2000');
-    expect($accounts[1]->type)->toEqual(AccountType::Liability);
-    expect($accounts[1]->name)->toBe('Liabilities');
-
-    expect($accounts[2]->number)->toBe('3000');
-    expect($accounts[2]->type)->toEqual(AccountType::Equity);
-    expect($accounts[2]->name)->toBe('Equity');
-
-    expect($accounts[3]->number)->toBe('4000');
-    expect($accounts[3]->type)->toEqual(AccountType::Revenue);
-    expect($accounts[3]->name)->toBe('Income');
-
-    expect($accounts[4]->number)->toBe('5000');
-    expect($accounts[4]->type)->toEqual(AccountType::Expense);
-    expect($accounts[4]->name)->toBe('Expenses');
+    foreach ($accountAttributes as $index => [$number, $type, $name]) {
+        expect($accounts[$index]->number)->toBe($number);
+        expect($accounts[$index]->type)->toBe($type);
+        expect($accounts[$index]->name)->toBe($name);
+    }
 });
