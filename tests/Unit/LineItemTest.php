3ea9bddb5226b6f6fcbdd19fcb0279a136275502
Zeb DeOs <zebulin@stechstudio.com>
2023-07-11 10:37:20 -0400

    LineItem: finish testing ::isDebit() and test ::isCredit()

--- tests/Unit/LineItemTest.php
+++ tests/Unit/LineItemTest.php
@@ -6,5 +6,24 @@ use STS\Beankeep\Models\LineItem;
 
 it('can correctly determine when it is a debit', function () {
     $lineItem = new LineItem(['debit' => 100, 'credit' => 0]);
+
     expect($lineItem->isDebit())->toBeTrue();
+});
+
+it('can correctly determine when it is not a debit', function () {
+    $lineItem = new LineItem(['debit' => 0, 'credit' => 100]);
+
+    expect($lineItem->isDebit())->toBeFalse();
+});
+
+it('can correctly determine when it is a credit', function () {
+    $lineItem = new LineItem(['debit' => 0, 'credit' => 100]);
+
+    expect($lineItem->isCredit())->toBeTrue();
+});
+
+it('can correctly determine when it is not a credit', function () {
+    $lineItem = new LineItem(['debit' => 100, 'credit' => 0]);
+
+    expect($lineItem->isCredit())->toBeFalse();
 });
