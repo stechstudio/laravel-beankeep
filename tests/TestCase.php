be66179e94ed1f9b6318162a5a4caf257267eae8
Zeb DeOs <zebulin@stechstudio.com>
2023-07-11 10:02:19 -0400

    TestCase: just use the RefreshDatabase trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,28 +4,18 @@ declare(strict_types=1);
 
 namespace STS\Beankeep\Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use STS\Beankeep\BeankeepServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->setUpDatabase();
-    }
+    use RefreshDatabase;
 
     protected function getPackageProviders($app): array
     {
         return [
             BeankeepServiceProvider::class,
         ];
-    }
-
-    protected function setUpDatabase(): void
-    {
-        // our library's migrations
-        $this->artisan('migrate')->run();
     }
 }
