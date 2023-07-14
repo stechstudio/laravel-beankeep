c1168ade4dc8167328b2f616495bbf285e209bf2
Zeb DeOs <zebulin@stechstudio.com>
2023-07-14 16:58:14 -0400

    HasLineItem: [wip] test the happy path

--- tests/Feature/HasLineItemTest.php
+++ tests/Feature/HasLineItemTest.php
@@ -7,14 +7,24 @@ namespace STS\Beankeep\Tests\Feature;
 use STS\Beankeep\Models\LineItem as BeankeepLineItem;
 use STS\Beankeep\Tests\TestCase;
 use STS\Beankeep\Tests\TestSupport\Models\Augmented\LineItem;
+use STS\Beankeep\Tests\TestSupport\Traits\BeanConstructors;
 
 final class HasLineItemTest extends TestCase
 {
+    use BeanConstructors;
+
     public function testItKnowsItsBeankeepClass(): void
     {
         $this->assertEquals(
             BeankeepLineItem::class,
             LineItem::beankeeperClass(),
         );
+    }
+
+    public function testItCanBeAssociatedWithAnEndUserLineItemModel(): void
+    {
+        $accounts = $this->createAccounts();
+
+        $this->assertTrue(false, 'TODO(zmd): finish me!');
     }
 }
