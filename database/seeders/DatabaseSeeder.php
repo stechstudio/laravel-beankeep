978a7d70a10e0ab71cf516c93e0099a6ca23703a
Zeb DeOs <zebulin@stechstudio.com>
2023-07-25 18:09:53 -0400

    DatabaseSeeder: seed source documents

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Collection;
 use STS\Beankeep\Enums\AccountType;
 use STS\Beankeep\Models\Account;
 use STS\Beankeep\Models\LineItem;
+use STS\Beankeep\Models\SourceDocument;
 use STS\Beankeep\Models\Transaction;
 
 class DatabaseSeeder extends Seeder
@@ -30,6 +31,11 @@ class DatabaseSeeder extends Seeder
         Transaction::all()->each(function (Transaction $transaction) {
             foreach ($this->randomUnsavedLineItems() as $lineItem) {
                 $transaction->lineItems()->save($lineItem);
+            }
+
+            if ($this->shouldHaveSourceDocument()) {
+                $sourceDocument = SourceDocument::factory()->make();
+                $transaction->sourceDocuments()->save($sourceDocument);
             }
 
             if ($this->shouldPostTransaction()) {
@@ -58,6 +64,11 @@ class DatabaseSeeder extends Seeder
     public function amount(): int
     {
         return $this->faker->numberBetween(1, 1500) * 100;
+    }
+
+    public function shouldHaveSourceDocument(): bool
+    {
+        return $this->faker->numberBetween(1, 5) % 5 != 0;
     }
 
     public function shouldPostTransaction(): bool
