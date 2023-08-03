a2dd07333196f0f737840bb5ab63b3f729d142f3
Zeb DeOs <zebulin@stechstudio.com>
2023-08-03 16:54:17 -0400

    StaticSeeder: remove unused import

--- database/seeders/StaticSeeder.php
+++ database/seeders/StaticSeeder.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace STS\Beankeep\Database\Seeders;
 
-use Carbon\CarbonPeriod;
 use Illuminate\Support\Carbon;
 use STS\Beankeep\Database\Factories\Support\HasRelativeTransactor;
 use STS\Beankeep\Database\Factories\Support\Transactor;
