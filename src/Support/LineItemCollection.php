7e4407ea6bf107a9e91eb03f64550a9888829bab
Zeb DeOs <zebulin@stechstudio.com>
2023-08-08 15:15:32 -0400

    LineItemCollection: [todo] stub out some extra helper methods

--- src/Support/LineItemCollection.php
+++ src/Support/LineItemCollection.php
@@ -7,16 +7,38 @@ namespace STS\Beankeep\Support;
 use Illuminate\Database\Eloquent\Collection;
 use STS\Beankeep\Models\Account;
 
-// TODO(zmd): test me
 class LineItemCollection extends Collection
 {
+    // TODO(zmd): test me
     public function debits(): Collection
     {
         return $this->filter->isDebit();
     }
 
+    // TODO(zmd): test me
     public function credits(): Collection
     {
         return $this->filter->isCredit();
+    }
+
+    // TODO(zmd): test me
+    public function getSums(): array
+    {
+        return [
+            $this->sumDebits(),
+            $this->sumCredits(),
+        ];
+    }
+
+    // TODO(zmd): test me
+    public function subDebits(): int
+    {
+        // TODO(zmd): implement me
+    }
+
+    // TODO(zmd): test me
+    public function subCredits(): int
+    {
+        // TODO(zmd): implement me
     }
 }
