9dd7f3ec902afa7de9bf73e0b888413cf74840da
Zeb DeOs <zebulin@stechstudio.com>
2023-08-09 14:08:23 -0400

    LineItem: test that ::ledgerEntries() scope works as expected for the default period

---
+++ tests/Feature/LineItem/LineItemScopeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace STS\Beankeep\Tests\Feature\LineItem;
+
+use STS\Beankeep\Models\LineItem;
+use STS\Beankeep\Tests\TestCase;
+use STS\Beankeep\Tests\TestSupport\Traits\HasDefaultTransactions;
+
+final class LineItemScopeTest extends TestCase
+{
+    use HasDefaultTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->threeMonthsOfTransactions();
+    }
+
+    // -- ::scopeLedgerEntries() ----------------------------------------------
+
+    public function testLedgerEntriesIncludesOnlyPostedEntriesForDefaultPeriod(): void
+    {
+        $this->assertEquals(10, LineItem::ledgerEntries()->count());
+        $this->assertEquals(690500, LineItem::ledgerEntries()->sum('debit'));
+        $this->assertEquals(690500, LineItem::ledgerEntries()->sum('credit'));
+    }
+
+    // TODO(zmd): public function testLedgerEntriesIncludesOnlyPostedEntriesForSpecifiedPeriod(): void {}
+
+    // -- ::scopePeriod() -----------------------------------------------------
+
+    // -- ::scopePriorTo() ----------------------------------------------------
+
+    // -- ::scopePosted() -----------------------------------------------------
+
+    // -- ::scopePending() ----------------------------------------------------
+
+    // -- ::scopeDebits() -----------------------------------------------------
+
+    // -- ::scopeCredits() ----------------------------------------------------
+}
