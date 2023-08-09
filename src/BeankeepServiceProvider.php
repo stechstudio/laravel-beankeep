4e1f9f4430f3387fd4399d18da7a4c0ce7b41305
Zeb DeOs <zebulin@stechstudio.com>
2023-08-09 09:03:17 -0400

    BeankeepServiceProvider: we need a proper path to configs we want to publish

--- src/BeankeepServiceProvider.php
+++ src/BeankeepServiceProvider.php
@@ -9,7 +9,7 @@ class BeankeepServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '../config/beankeep.php' => config_path('beankeep.php'),
+            __DIR__ . '/../config/beankeep.php' => config_path('beankeep.php'),
         ]);
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
