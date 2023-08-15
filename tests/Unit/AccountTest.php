a36cc0e94058b698eb08ab9ef61b0cb691fe51a7
Zeb DeOs <zebulin@stechstudio.com>
2023-08-15 11:17:59 -0400

    AccountTest: stub out new test case

---
+++ tests/Unit/AccountTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace STS\Beankeep\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use STS\Beankeep\Enums\AccountType;
+use STS\Beankeep\Models\Account;
+
+final class AccountTest extends TestCase
+{
+    // -- ::debitPositive() ---------------------------------------------------
+
+    // TODO(zmd): write tests for ::debitPositive()
+    public function testTruth(): void
+    {
+        $this->assertTrue(false);
+    }
+
+    // -- ::creditPositive() --------------------------------------------------
+
+    // TODO(zmd): write tests for ::creditPositive()
+}
