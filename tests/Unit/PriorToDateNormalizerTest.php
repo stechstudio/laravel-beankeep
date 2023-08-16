628050cb41cfea1e2fd9e3897ced154c7fd10b24
Zeb DeOs <zebulin@stechstudio.com>
2023-08-16 09:50:15 -0400

    PriorToDateNormalizer: test that ::normalize() returns mutable start date from given period

--- tests/Unit/PriorToDateNormalizerTest.php
+++ tests/Unit/PriorToDateNormalizerTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace STS\Beankeep\Tests\Unit;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Support\Carbon;
 use PHPUnit\Framework\TestCase;
 use STS\Beankeep\Support\PriorToDateNormalizer;
 
@@ -16,7 +17,17 @@ final class PriorToDateNormalizerTest extends TestCase
 
     // TODO(zmd): public function testNormalizeReturnsImmutableDateParsedFromGivenString(): void {}
 
-    // TODO(zmd): public function testNormalizeReturnsMutableStartDateOfGivenPeriod(): void {}
+    public function testNormalizeReturnsMutableStartDateOfGivenPeriod(): void
+    {
+        $startDate = Carbon::now();
+        $endDate = Carbon::now()->addDays(5);
+        $period = $startDate->daysUntil($endDate);
+
+        $normalized = PriorToDateNormalizer::normalize($period);
+
+        $this->assertEquals($startDate, $normalized);
+        $this->assertInstanceOf(Carbon::class, $normalized);
+    }
 
     public function testNormalizeReturnsImmutableStartDateOfGivenPeriod(): void
     {
