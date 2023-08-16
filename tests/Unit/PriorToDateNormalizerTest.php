a7fa8b1795febd36143310fdd227a89b020f6967
Zeb DeOs <zebulin@stechstudio.com>
2023-08-16 09:33:40 -0400

    PriorToDateNormalizerTest: stub out test case

---
+++ tests/Unit/PriorToDateNormalizerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace STS\Beankeep\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use STS\Beankeep\Support\PriorToDateNormalizer;
+
+final class PriorToDateNormalizerTest extends TestCase
+{
+    public function testTruth(): void
+    {
+        $this->assertTrue(false);
+    }
+}
