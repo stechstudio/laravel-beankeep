8e873d8fd68cbe8a85b8ae9e19f22882aacd6f55
Zeb DeOs <zebulin@stechstudio.com>
2023-08-16 14:52:56 -0400

    config/beankeep: document the default-period option

--- config/beankeep.php
+++ config/beankeep.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    // TODO(zmd): document me
+    /*
+     * Default period to use for annual reports & year-end calculations.
+     */
     'default-period' => ['1-jan', '31-dec'],
 ];
