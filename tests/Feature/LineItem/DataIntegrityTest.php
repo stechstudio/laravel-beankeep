8b9bc6d35d98ea65ad10876003cd863d933661ec
Zeb DeOs <zebulin@stechstudio.com>
2023-08-17 11:15:30 -0400

    DataIntegrityTest: expect the expected

--- tests/Feature/LineItem/DataIntegrityTest.php
+++ tests/Feature/LineItem/DataIntegrityTest.php
@@ -6,6 +6,7 @@ namespace STS\Beankeep\Tests\Feature\LineItem;
 
 use Illuminate\Support\Carbon;
 use STS\Beankeep\Enums\AccountType;
+use STS\Beankeep\Exceptions\LineItemInvalid;
 use STS\Beankeep\Models\Account;
 use STS\Beankeep\Models\LineItem;
 use STS\Beankeep\Models\Transaction;
@@ -65,31 +66,29 @@ final class DataIntegrityTest extends TestCase
         $this->assertTrue($lineItem->isCredit());
     }
 
-    // TODO(zmd): update to reflect desire to raise not silently fail a save
     public function testRefusesToCreateWithBothCreditAndDebitAmount(): void
     {
+        $this->expectException(LineItemInvalid::class);
+
         $lineItem = LineItem::create([
             'account_id' => $this->account()->id,
             'transaction_id' => $this->transaction()->id,
             'debit' => 10000,
             'credit' => 10000,
         ]);
-
-        $this->assertFalse($lineItem->exists);
     }
 
-    // TODO(zmd): update to reflect desire to raise not silently fail a save
     public function testRefusesToSaveWithBothCreditAndDebitAmount(): void
     {
         $lineItem = new LineItem(['debit' => 10000, 'credit' => 10000]);
         $lineItem->account_id = $this->account()->id;
         $lineItem->transaction_id = $this->transaction()->id;
 
-        $this->assertFalse($lineItem->save());
-        $this->assertFalse($lineItem->exists);
+        $this->expectException(LineItemInvalid::class);
+
+        $lineItem->save();
     }
 
-    // TODO(zmd): update to reflect desire to raise not silently fail a save
     public function testRefusesToUpdateWithBothCreditAndDebitAmount(): void
     {
         $lineItem = new LineItem(['debit' => 10000, 'credit' => 0]);
@@ -101,35 +100,34 @@ final class DataIntegrityTest extends TestCase
 
         $lineItem->credit = 10000;
 
-        $this->assertFalse($lineItem->save());
-        $this->assertEquals(0, $lineItem->refresh()->credit);
+        $this->expectException(LineItemInvalid::class);
+
+        $lineItem->save();
     }
 
-    // TODO(zmd): update to reflect desire to raise not silently fail a save
     public function testRefusesToCreateWithoutEitherCreditOrDebitAmount(): void
     {
+        $this->expectException(LineItemInvalid::class);
+
         $lineItem = LineItem::create([
             'account_id' => $this->account()->id,
             'transaction_id' => $this->transaction()->id,
             'debit' => 0,
             'credit' => 0,
         ]);
-
-        $this->assertFalse($lineItem->exists);
     }
 
-    // TODO(zmd): update to reflect desire to raise not silently fail a save
     public function testRefusesToSaveWithoutEitherCreditOrDebitAmount(): void
     {
         $lineItem = new LineItem(['debit' => 0, 'credit' => 0]);
         $lineItem->account_id = $this->account()->id;
         $lineItem->transaction_id = $this->transaction()->id;
 
-        $this->assertFalse($lineItem->save());
-        $this->assertFalse($lineItem->exists);
+        $this->expectException(LineItemInvalid::class);
+
+        $lineItem->save();
     }
 
-    // TODO(zmd): update to reflect desire to raise not silently fail a save
     public function testRefusesToUpdateWithoutEitherCreditOrDebitAmount(): void
     {
         $lineItem = new LineItem(['debit' => 10000, 'credit' => 0]);
@@ -141,8 +139,9 @@ final class DataIntegrityTest extends TestCase
 
         $lineItem->debit = 0;
 
-        $this->assertFalse($lineItem->save());
-        $this->assertEquals(10000, $lineItem->refresh()->debit);
+        $this->expectException(LineItemInvalid::class);
+
+        $lineItem->save();
     }
 
     // ------------------------------------------------------------------------
