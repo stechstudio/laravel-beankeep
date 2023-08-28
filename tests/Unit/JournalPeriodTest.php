228a59c6b1b38cd7a47f50939e839b7b9610254b
Zeb DeOs <zebulin@stechstudio.com>
2023-08-28 12:00:20 -0400

    JournalPeriodTest: stub out unit test case

---
+++ tests/Unit/JournalPeriodTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace STS\Beankeep\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class JournalPeriodTest extends TestCase
+{
+    public function testTruth(): void
+    {
+        $this->assertTrue(false);
+    }
+}
