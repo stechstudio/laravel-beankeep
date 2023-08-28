9b88d3dd481be2082ef01928cf90f4b1e96f68c7
Zeb DeOs <zebulin@stechstudio.com>
2023-08-28 14:20:39 -0400

    JournalPeriod: test ::toCarbonPeriod() correctly calculates start day in prior year

--- tests/Unit/JournalPeriodTest.php
+++ tests/Unit/JournalPeriodTest.php
@@ -19,24 +19,34 @@ final class JournalPeriodTest extends TestCase
 
     public function testToCarbonPeriod(): void
     {
-        // TODO(zmd): we need to use time travel to ensure we have a consistent
-        //   frame of reference
         $this->travelTo('2023-08-28');
 
-        $janPeriod = JournalPeriod::Jan->toCarbonPeriod();
+        $period = JournalPeriod::Jan->toCarbonPeriod();
 
-        $this->assertInstanceOf(CarbonPeriod::class, $janPeriod);
-        $this->assertInstanceOf(CarbonImmutable::class, $janPeriod->startDate);
-        $this->assertInstanceOf(CarbonImmutable::class, $janPeriod->endDate);
-        $this->assertEquals(CarbonImmutable::parse('2023-01-01'), $janPeriod->startDate);
-        $this->assertEquals(CarbonImmutable::parse('2023-12-31')->endOfDay(), $janPeriod->endDate);
+        $this->assertInstanceOf(CarbonPeriod::class, $period);
+        $this->assertInstanceOf(CarbonImmutable::class, $period->startDate);
+        $this->assertInstanceOf(CarbonImmutable::class, $period->endDate);
+        $this->assertEquals(CarbonImmutable::parse('2023-01-01'), $period->startDate);
+        $this->assertEquals(CarbonImmutable::parse('2023-12-31')->endOfDay(), $period->endDate);
     }
 
-    // TODO(zmd): test that start date is always before or same as current date
+    public function testToCarbonPeriodReturnsStartDateForPriorYear(): void
+    {
+        $this->travelTo('2023-04-01');
 
-    // TODO(zmd): test that end date is always after or same as current date
+        $period = JournalPeriod::May->toCarbonPeriod();
 
-    // TODO(zmd): test that leap-year as expected when end date is 29-feb
+        $this->assertEquals(CarbonImmutable::parse('2022-05-01'), $period->startDate);
+        $this->assertEquals(CarbonImmutable::parse('2023-04-30')->endOfDay(), $period->endDate);
+    }
+
+    // TODO(zmd): public function testToCarbonPeriodReturnsStartDateMatchingTheCurrentDate(): void {}
+
+    // TODO(zmd): public function testToCarbonPeriodReturnsEndDateAfterTheCurrentYear(): void {}
+
+    // TODO(zmd): public function testToCarbonPeriodReturnsEndDateMatchingTheCurrentDate(): void {}
+
+    // TODO(zmd): public function testToCarbonPeriodReturnsCorrectDateForLeapYear(): void {}
 
     // -- ::expanded() --------------------------------------------------------
 
