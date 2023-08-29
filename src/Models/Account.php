d66db073d23ff2a6be6a7caf54a8187338df31ad
Zeb DeOs <zebulin@stechstudio.com>
2023-08-29 12:10:15 -0400

    Journal: define relationship to Account

--- src/Models/Account.php
+++ src/Models/Account.php
@@ -6,6 +6,7 @@ namespace STS\Beankeep\Models;
 
 use Carbon\CarbonPeriod;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use STS\Beankeep\Database\Factories\AccountFactory;
 use STS\Beankeep\Enums\AccountType;
@@ -31,6 +32,11 @@ final class Account extends Beankeeper
     protected static function newFactory()
     {
         return AccountFactory::new();
+    }
+
+    public function journal(): BelongsTo
+    {
+        return $this->belongsTo(Journal::class);
     }
 
     public function lineItems(): HasMany
--- src/Models/Journal.php
+++ src/Models/Journal.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace STS\Beankeep\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use STS\Beankeep\Database\Factories\JournalFactory;
 use STS\Beankeep\Enums\JournalPeriod;
 
@@ -25,5 +26,10 @@ final class Journal extends Beankeeper
     protected static function newFactory()
     {
         return JournalFactory::new();
+    }
+
+    public function accounts(): HasMany
+    {
+        return $this->hasMany(Account::class);
     }
 }
