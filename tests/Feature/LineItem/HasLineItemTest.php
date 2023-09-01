1d763a15b24a1c05881c337420c74a81ec1f3538
Zeb DeOs <zebulin@stechstudio.com>
2023-09-01 16:23:07 -0400

    HasLineItemTest: convert to using the GeneratesJournalData trait

--- tests/Feature/LineItem/HasLineItemTest.php
+++ tests/Feature/LineItem/HasLineItemTest.php
@@ -4,16 +4,14 @@ declare(strict_types=1);
 
 namespace STS\Beankeep\Tests\Feature\LineItem;
 
-use STS\Beankeep\Database\Factories\Support\HasRelativeTransactor;
 use STS\Beankeep\Models\LineItem as BeankeepLineItem;
 use STS\Beankeep\Tests\TestCase;
 use STS\Beankeep\Tests\TestSupport\Models\Augmented\LineItem;
-use STS\Beankeep\Tests\TestSupport\Traits\CanCreateAccounts;
+use STS\Beankeep\Tests\TestSupport\Traits\GeneratesJournalData;
 
 final class HasLineItemTest extends TestCase
 {
-    use CanCreateAccounts;
-    use HasRelativeTransactor;
+    use GeneratesJournalData;
 
     public function testItKnowsItsBeankeepClass(): void
     {
@@ -25,12 +23,11 @@ final class HasLineItemTest extends TestCase
 
     public function testItCanBeAssociatedWithAnEndUserLineItemModel(): void
     {
-        $this->createAccounts();
-        $transaction = $this->thisYear('10/15')
-            ->transact('2 computers from computers-r-us')
-            ->line('equipment', dr: 5000.00)
-            ->line('accounts-payable', cr: 5000.00)
-            ->post();
+        $transaction = $this->txn(
+            '10/15/2023',
+            dr: ['equipment', 5000.00],
+            cr: ['accounts-payable', 5000.00],
+        );
 
         $debit = $transaction->lineItems()->where('debit', '>', 0)->first();
         $debitNarration = "On the 15th of October we did debit our Equipment account in the amount of $5000.00 for the acquisition of 2 new computers from Computers-R-Us";
